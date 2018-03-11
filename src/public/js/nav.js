6572ba81b040a4fced774ee3b28f817e931cc866
jane <jane@JiadeMacBook-Pro.local>
2018-03-11 13:51:43 -0700

    added nav listeners -not render

--- src/public/js/nav.js
+++ src/public/js/nav.js
@@ -1,5 +1,5 @@
-( () => {
 
+function toggleSideMenu(){
     let menulist = document.querySelectorAll(".menu-item");
     let submenulist = document.querySelectorAll(".sub-side-menu");
 
@@ -12,7 +12,9 @@
             }
         });
     }
+}
 
+function hideCornerSearch(){  // hide and display corner search
     let icon = document.querySelector('#search-icon');
     let bar = document.querySelector('#corner-search');
 
@@ -23,4 +25,28 @@
             bar.setAttribute('style', 'display:none');
         }
     });
-})();
+}
+
+const category = ['genre', 'author', 'title', 'popular'];
+
+ // add listeners to main menu
+function handleMainMenuClick(){
+    for(let i = 0; i < category.length; i++){
+        let item = document.querySelector('.item-'+ i);
+        item.addEventListener("click", () => {
+            let allItems = document.querySelectorAll('.classification');
+            let sideItem = document.querySelector('.c' + i);
+            for(let j = 0; j < allItems.length; j++){
+                allItems[j].setAttribute('style', 'display: none');
+            }
+            sideItem.setAttribute('style', 'display: block');
+            console.log("click menu: " + category[i]);
+        });
+    }
+}
+
+handleMainMenuClick();
+toggleSideMenu();
+hideCornerSearch();
+
+
